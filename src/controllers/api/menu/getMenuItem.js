e6c989cb7d71ecc9103283b5fe1dad91f27a4a36
Vladimir Tribusean <vladimir.tribusean@gmail.com>
2018-01-08 04:28:05 +0200

    added getMenuItem controller

---
+++ src/controllers/api/menu/getMenuItem.js
@@ -0,0 +1,24 @@
+import Api from '../../../lib/api';
+
+export default (request, response) => {
+  const { menuId } = request.params;
+
+  if (!menuId) {
+    return response.status(400).send('You must provide id of the menu you want to validate');
+  }
+
+  const onSuccess = menu => {
+    response.send(menu);
+  };
+
+  const onError = error => {
+    if (error.response) {
+      const { status, data } = error.response;
+      return response.status(status).send(data);
+    }
+
+    return response.status(500).send(error);
+  };
+
+  return Api.getMenyById(menuId).then(onSuccess).catch(onError);
+}
